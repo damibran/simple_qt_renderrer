bf85394dd1d69007d367c2dd222debdffa798c49
damibran <olegevdk@gmail.com>
2022-02-14 23:18:00 +0300

    move to widgets project

--- utils/MVP_mat.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include<glm/glm.hpp>
-
-struct MVP_mat
-{
-	glm::mat4 model = glm::mat4(1);
-	glm::mat4 view;
-	glm::mat4 proj;
-};
--- utils/Vertex.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-#include <glm/glm.hpp>
-
-struct vertex
-{
-	glm::vec3 pos;
-	glm::vec3 norm;
-};
