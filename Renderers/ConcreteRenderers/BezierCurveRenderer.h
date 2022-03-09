9f7c4534ec8e17fcbe199dab84334f822a05614f
damibran <olegevdk@gmail.com>
2022-03-09 21:30:07 +0300

    little map.instert tweak and cleaning

--- Renderers/ConcreteRenderers/BezierCurveRenderer.h
+++ Renderers/ConcreteRenderers/BezierCurveRenderer.h
@@ -61,9 +61,9 @@ public:
 			auto left = curve_patch_.find({start, centr});
 			if (left == curve_patch_.end())
 			{
-				left = curve_patch_.insert({
+				left = curve_patch_.insert(cur,{
 					{start, centr}, {{decasteljau(start), decasteljau(centr)}, last_update}
-				}).first;
+				});
 			}
 			if (left->second.second < last_update)
 			{
@@ -74,9 +74,9 @@ public:
 			auto right = curve_patch_.find({centr, end});
 			if (right == curve_patch_.end())
 			{
-				right = curve_patch_.insert({
+				right = curve_patch_.insert(cur,{
 					{centr, end}, {{decasteljau(centr), decasteljau(end)}, last_update}
-				}).first;
+				});
 			}
 			if (right->second.second < last_update)
 			{
@@ -122,63 +122,6 @@ public:
 	}
 
 private:
-	bool intersectScreenRectangle(LineSeg seg)
-	{
-		return doIntersect({0, 0}, {screen_.XMAX, 0}, seg.a, seg.b) ||
-			doIntersect({0, 0}, {0, screen_.YMAX}, seg.a, seg.b) ||
-			doIntersect({0, screen_.YMAX}, {screen_.XMAX, screen_.YMAX}, seg.a, seg.b) ||
-			doIntersect({screen_.XMAX, 0}, {screen_.XMAX, screen_.YMAX}, seg.a, seg.b);
-	}
-
-	bool onSegment(glm::vec2 p, glm::vec2 q, glm::vec2 r)
-	{
-		if (q.x <= std::max(p.x, r.x) && q.x >= std::min(p.x, r.x) &&
-			q.y <= std::max(p.y, r.y) && q.y >= std::min(p.y, r.y))
-			return true;
-
-		return false;
-	}
-
-	int orientation(glm::vec2 p, glm::vec2 q, glm::vec2 r)
-	{
-		// See https://www.geeksforgeeks.org/orientation-3-ordered-points/
-		// for details of below formula.
-		int val = (q.y - p.y) * (r.x - q.x) -
-			(q.x - p.x) * (r.y - q.y);
-
-		if (val == 0) return 0; // collinear
-
-		return (val > 0) ? 1 : 2; // clock or counterclock wise
-	}
-
-	bool doIntersect(glm::vec2 p1, glm::vec2 q1, glm::vec2 p2, glm::vec2 q2)
-	{
-		// Find the four orientations needed for general and
-		// special cases
-		int o1 = orientation(p1, q1, p2);
-		int o2 = orientation(p1, q1, q2);
-		int o3 = orientation(p2, q2, p1);
-		int o4 = orientation(p2, q2, q1);
-
-		// General case
-		if (o1 != o2 && o3 != o4)
-			return true;
-
-		// Special Cases
-		// p1, q1 and p2 are collinear and p2 lies on segment p1q1
-		if (o1 == 0 && onSegment(p1, p2, q1)) return true;
-
-		// p1, q1 and q2 are collinear and q2 lies on segment p1q1
-		if (o2 == 0 && onSegment(p1, q2, q1)) return true;
-
-		// p2, q2 and p1 are collinear and p1 lies on segment p2q2
-		if (o3 == 0 && onSegment(p2, p1, q2)) return true;
-
-		// p2, q2 and q1 are collinear and q1 lies on segment p2q2
-		if (o4 == 0 && onSegment(p2, q1, q2)) return true;
-
-		return false; // Doesn't fall in any of the above cases
-	}
 
 	bool xInBounds(glm::vec2 a)
 	{
