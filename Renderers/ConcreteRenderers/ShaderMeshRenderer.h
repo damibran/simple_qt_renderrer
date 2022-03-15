74d6c9d7c6ec4856926e0f9bd437570880e43c04
damibran <olegevdk@gmail.com>
2022-03-15 19:37:34 +0300

    problem solved

--- Renderers/ConcreteRenderers/ShaderMeshRenderer.h
+++ Renderers/ConcreteRenderers/ShaderMeshRenderer.h
@@ -8,7 +8,7 @@ class ShaderMeshRenderer final : public RendererComponent
 {
 public:
 	ShaderMeshRenderer(Screen& s, std::unique_ptr<Shader> shdr, std::unique_ptr<Mesh> m) : screen_(s),
-		shader_(std::move(shdr)), mesh_(std::move(m)), pool_(1) { pool_.sleep_duration = 0; }
+		shader_(std::move(shdr)), mesh_(std::move(m)), pool_(4) { pool_.sleep_duration = 0; }
 
 	void drawShapeVisual(const MVPMat& trans) override
 	{
@@ -66,61 +66,62 @@ private:
 		const float xmax = max3(v0.x, v1.x, v2.x);
 		const float ymax = max3(v0.y, v1.y, v2.y);
 
-		if (xmin < static_cast<float>(screen_.XMAX - 1) && xmax > 0 && ymin < static_cast<float>(screen_.YMAX - 1) && ymax > 0)
+		if (xmin > screen_.XMAX - 1 || xmax < 0 || ymin > screen_.YMAX || ymax < 0)
+			return;
+
+		// be careful xmin/xmax/ymin/ymax can be negative. Don't cast to uint32_t
+		uint x0 = std::max(0, static_cast<int>(std::floor(xmin)));
+		uint x1 = std::min(static_cast<int>(screen_.XMAX - 1), static_cast<int>(std::floor(xmax)));
+		const uint y0 = std::max(1, static_cast<int>(std::floor(ymin)));
+		const uint y1 = std::min(static_cast<int>(screen_.YMAX - 1), static_cast<int>(std::floor(ymax)));
+
+		float area = edgeFunction(v0, v1, v2);
+		auto loop = [this, v0, v1, v2, area, x0, x1, &shader](const uint a, const uint b)
 		{
-			uint x0 = std::max(0u, static_cast<uint>(std::floor(xmin)));
-			uint x1 = std::min(screen_.XMAX - 1, static_cast<uint>(std::floor(xmax)));
-			const uint y0 = std::max(1u, static_cast<uint>(std::floor(ymin)));
-			const uint y1 = std::min(screen_.YMAX - 1, static_cast<uint>(std::floor(ymax)));
-
-			float area = edgeFunction(v0, v1, v2);
-			auto loop = [this, v0, v1, v2, area, x0, x1, &shader](const uint a, const uint b)
+			for (uint y = a; y <= b; ++y)
 			{
-				for (uint y = a; y <= b; ++y)
+				for (uint x = x0; x <= x1; ++x)
 				{
-					for (uint x = x0; x <= x1; ++x)
+					glm::vec2 pixel = {x + 0.5, y + 0.5};
+
+					float w0 = edgeFunction(v1, v2, pixel);
+					float w1 = edgeFunction(v2, v0, pixel);
+					float w2 = edgeFunction(v0, v1, pixel);
+
+					if (w0 >= 0 && w1 >= 0 && w2 >= 0)
 					{
-						glm::vec2 pixel = {x + 0.5, y + 0.5};
+						w0 /= area;
+						w1 /= area;
+						w2 /= area;
 
-						float w0 = edgeFunction(v1, v2, pixel);
-						float w1 = edgeFunction(v2, v0, pixel);
-						float w2 = edgeFunction(v0, v1, pixel);
+						const float corr = w0 / v0.z + w1 / v1.z + w2 / v2.z;
 
-						if (w0 >= 0 && w1 >= 0 && w2 >= 0)
+						w0 /= v0.z;
+						w1 /= v1.z;
+						w2 /= v2.z;
+
+						w0 /= corr;
+						w1 /= corr;
+						w2 /= corr;
+
+						const float z = w0 * v0.z + w1 * v1.z + w2 * v2.z;
+
+						if (z < screen_.getZBufferAt(y * screen_.XMAX + x))
 						{
-							w0 /= area;
-							w1 /= area;
-							w2 /= area;
+							screen_.setZBufferAt(y * screen_.XMAX + x, z);
 
-							const float corr = w0 / v0.z + w1 / v1.z + w2 / v2.z;
+							glm::vec3 color = shader->computeFragmentShader(pixel, w0, w1, w2);
 
-							w0 /= v0.z;
-							w1 /= v1.z;
-							w2 /= v2.z;
-
-							w0 /= corr;
-							w1 /= corr;
-							w2 /= corr;
-
-							const float z = w0 * v0.z + w1 * v1.z + w2 * v2.z;
-
-							if (z < screen_.getZBufferAt(y * screen_.XMAX + x))
-							{
-								screen_.setZBufferAt(y * screen_.XMAX + x, z);
-
-								glm::vec3 color = shader->computeFragmentShader(pixel, w0, w1, w2);
-
-								color.r = glm::clamp<float>(color.r, 0, 255);
-								color.g = glm::clamp<float>(color.g, 0, 255);
-								color.b = glm::clamp<float>(color.b, 0, 255);
-								screen_.put_point(x, y, color);
-							}
+							color.r = glm::clamp<float>(color.r, 0, 255);
+							color.g = glm::clamp<float>(color.g, 0, 255);
+							color.b = glm::clamp<float>(color.b, 0, 255);
+							screen_.put_point(x, y, color);
 						}
 					}
 				}
-			};
-			pool_.parallelize_loop(y0, y1, loop, (y1 - y0) / pool_.get_thread_count());
-		}
+			}
+		};
+		pool_.parallelize_loop(y0, y1, loop, (y1 - y0) / pool_.get_thread_count());
 	}
 
 	Screen& screen_;
